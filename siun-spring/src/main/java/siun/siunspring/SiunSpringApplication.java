e9c60a06a8b834e174f3e149b0e9c4d73ae021f5
xilucks <siun0331@gmail.com>
2021-12-12 13:10:45 +0900

    환경설정 완료

---
+++ siun-spring/src/main/java/siun/siunspring/SiunSpringApplication.java
@@ -0,0 +1,13 @@
+package siun.siunspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiunSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiunSpringApplication.class, args);
+	}
+
+}
---
+++ siun-spring/src/test/java/siun/siunspring/SiunSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package siun.siunspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiunSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
