02b98111038b9221f2dca32b9d0b94ad434fa5ec
xilucks <siun0331@gmail.com>
2021-12-16 01:26:05 +0900

    오류 수정 및 메소드 추가

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,12 +1,9 @@
 package hello.hellospring.repository;
-
-import java.lang.reflect.Member;
+import hello.hellospring.domain.Member;
 import java.util.List;
 import java.util.Optional;
-
-public interface MemberRepostory {
-    Member save(Member member);
-    Optional<Member> findById(Long id);
+public interface MemberRepository {
+    Member save(Member member); Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
     List<Member> findAll();
 }
