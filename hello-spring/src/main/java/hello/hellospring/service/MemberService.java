2891cb34a7cdd7dd0ef6425b4176f628d26789bb
xilucks <siun0331@gmail.com>
2021-12-18 20:46:37 +0900

    회원 서비스 생성

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,10 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+
+}
