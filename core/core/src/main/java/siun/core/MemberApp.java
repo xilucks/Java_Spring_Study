080b448f3e470fd0941ef152d358b16b3408ac67
xilucks <siun0331@gmail.com>
2022-01-10 01:05:16 +0900

    도메인 테스트

---
+++ core/core/src/main/java/siun/core/MemberApp.java
@@ -0,0 +1,19 @@
+package siun.core;
+
+import siun.core.member.Grade;
+import siun.core.member.Member;
+import siun.core.member.MemberService;
+import siun.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+
+    }
+}
