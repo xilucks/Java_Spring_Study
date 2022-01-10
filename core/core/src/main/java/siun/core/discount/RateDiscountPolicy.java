f34251a5574ff86861854a0299e93cea8807da1a
xilucks <siun0331@gmail.com>
2022-01-11 00:06:34 +0900

    새로운 할인 정책 개발

---
+++ core/core/src/main/java/siun/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package siun.core.discount;
+
+import siun.core.member.Grade;
+import siun.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    private int discountPercent = 10;
+
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return price * discountPercent /100;
+        }else{
+            return 0;
+        }
+    }
+}
