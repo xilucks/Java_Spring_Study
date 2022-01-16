3f2670af305e2672a5803817c5a0c7eda0e3a4c1
xilucks <siun0331@gmail.com>
2022-01-17 01:31:58 +0900

    singleton pattern

--- core/core/src/test/java/siun/core/singleton/SingletonTest.java
+++ core/core/src/test/java/siun/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import siun.core.AppConfig;
 import siun.core.member.MemberService;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SingletonTest {
 
     @Test
@@ -23,8 +25,20 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         //memberService != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
 
     }
 
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonSerticeTest(){
+        SingletonService instance1 = SingletonService.getInstance();
+        SingletonService instance2 = SingletonService.getInstance();
+
+        System.out.println("instance1 = " + instance1);
+        System.out.println("instance2 = " + instance2);
+
+        assertThat(instance1).isSameAs(instance2);
+    }
 }
