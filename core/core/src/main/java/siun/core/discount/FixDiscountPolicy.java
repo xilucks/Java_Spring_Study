718b9161c2fa0a7a0c2bba5c3554a2f423363c8c
xilucks <siun0331@gmail.com>
2022-01-25 00:17:45 +0900

    bean List, Map

--- core/core/src/main/java/siun/core/discount/FixDiscountPolicy.java
+++ core/core/src/main/java/siun/core/discount/FixDiscountPolicy.java
@@ -1,10 +1,12 @@
 package siun.core.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import siun.core.member.Grade;
 import siun.core.member.Member;
 
 @Component
+@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000;
