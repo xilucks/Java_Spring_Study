40f8d69d40a1ba5d91389dedd7b557bdac626b6e
xilucks <siun0331@gmail.com>
2022-01-26 23:31:58 +0900

    bean lifecycle callback

---
+++ core/core/src/test/java/siun/core/lifecycle/NetworkClient.java
@@ -0,0 +1,29 @@
+package siun.core.lifecycle;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("init connect message");
+    }
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    public void connect(){
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call: "+ url + " message = " + message);
+    }
+
+    public void disconnect(){
+        System.out.println("close: "+ url);
+    }
+
+}
