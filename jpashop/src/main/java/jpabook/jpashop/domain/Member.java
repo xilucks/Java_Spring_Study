c40f1a95c159d6877d749f1e4f3383f6f0b2c98e
xilucks <siun0331@gmail.com>
2022-03-01 14:29:48 +0900

    entitiy setting

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,19 +1,28 @@
 package jpabook.jpashop.domain;
 
-import org.apache.tomcat.jni.Address;
+import lombok.Getter;
+import lombok.Setter;
+import jpabook.jpashop.domain.Address;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 import javax.persistence.criteria.Order;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Member {
+
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
+
     private String  name;
+
+    @Embedded
     private Address address;
+
+    @OneToMany
     private List<Order> orders = new ArrayList<>();
 }
