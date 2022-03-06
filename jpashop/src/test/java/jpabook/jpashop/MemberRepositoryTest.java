431ed5ee63375d3ec8dccacb1b93f6f314915841
xilucks <siun0331@gmail.com>
2022-03-06 21:13:49 +0900

    MemberRepository add

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,37 +1,38 @@
-package jpabook.jpashop;
-
-import jpabook.jpashop.domain.Member;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-class MemberRepositoryTest {
-
-    @Autowired MemberRepository memberRepository;
-    //작동 테스트
-    @Test
-    @Transactional
-    public void testMember() throws Exception{
-        //given
-        Member member = new Member();
-        member.setName("userA");
-
-
-        //when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
-
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
-    }
-
-}
+//package jpabook.jpashop;
+//
+//import jpabook.jpashop.domain.Member;
+//import jpabook.jpashop.repository.MemberRepository;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//class MemberRepositoryTest {
+//
+//    @Autowired MemberRepository memberRepository;
+//    //작동 테스트
+//    @Test
+//    @Transactional
+//    public void testMember() throws Exception{
+//        //given
+//        Member member = new Member();
+//        member.setName("userA");
+//
+//
+//        //when
+////        Long savedId = memberRepository.save(member);
+////        Member findMember = memberRepository.findOne(savedId);
+//
+//        //then
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+//    }
+//
+//}
