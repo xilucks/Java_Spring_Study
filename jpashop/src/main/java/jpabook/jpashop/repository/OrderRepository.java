ee7a388f3994126ac042ab983c9c1a2481c7ab00
xilucks <siun0331@gmail.com>
2022-03-08 01:06:47 +0900

    orderrepository dev

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.Order;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order){
+        em.persist(order);
+    }
+
+    public Order findOne(Long id){
+        return em.find(Order.class,id);
+    }
+
+    //public List<Order> findAll(OrderSerach orderSerach){}
+
+}
