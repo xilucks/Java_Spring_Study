0e0ad6ff114b26c8dcc30b3d24e266b7d79f5f02
xilucks <siun0331@gmail.com>
2022-03-11 23:49:23 +0900

    member add dev

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.NotEmpty;
 @Getter @Setter
 public class MemberForm {
 
-    @NotEmpty
+    @NotEmpty(message = "회원 이름은 필수 입니다")
     private String name;
 
     private String city;
