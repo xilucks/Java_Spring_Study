f5193e67a9f1492782e15208c0b1cd358ee8862a
xilucks <siun0331@gmail.com>
2022-07-10 18:35:54 +0900

    자바 스프링 기초편 완강

---
+++ siun/src/main/java/hello/siun/web/LogDemoController.java
@@ -0,0 +1,30 @@
+package hello.siun.web;
+
+import hello.siun.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.*;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){
+        String requestURL = request.getRequestURL().toString();
+//        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
---
+++ siun/src/main/java/hello/siun/web/LogDemoService.java
@@ -0,0 +1,20 @@
+package hello.siun.web;
+
+import hello.siun.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    private final MyLogger myLogger;
+
+    public void logic(String testId) {
+//        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service Id : " + testId);
+    }
+}
